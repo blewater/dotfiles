53650a07255754afdd017c4ec8a47962a46d1864
User <user@Users-MacBook-Pro.local>
2019-10-09 12:38:02 +0300

    Remove Powerline setup, add vim line numbers

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,36 @@
 set -Ux LC_CTYPE en_US.UTF-8
 set -Ux LC_ALL en_US.UTF-8
 set -Ux LANG en_US.UTF-8
+set -Ux GOPATH $HOME/go
 
-set fish_function_path $fish_function_path ~/Library/Python/2.7/lib/python/site-packages/powerline/bindings/fish
-powerline-setup
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+set -g theme_nerd_fonts yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_git_worktree_support yes
+set -g theme_display_virtualenv no
+set -g theme_display_ruby no
+set -g theme_display_user yes
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+set -g theme_date_format "+%a %b %-d %H:%M"
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g default_user your_normal_user
+set -g fish_prompt_pwd_dir_length 0
+#set -g theme_project_dir_length 1
 
 if status is-interactive 
 and not set -q TMUX
     exec tmux
 end
 
-alias ls='ls -laFh'
-alias ll='ls -alFh'
+alias l='ls -alFh'
 alias la='ls -AFh'
-alias l='ls -CF'
+alias lc='ls -CF'
 alias vi='vim'
 alias s0='shutdown -r +0'
 export EDITOR=vim
 
-source ~/.iterm2_shell_integration.(basename $SHELL)
 alias config='/usr/bin/git --git-dir=/Users/user/.config/ --work-tree=/Users/user'
--- .vimrc
+++ .vimrc
@@ -2,3 +2,7 @@ syntax enable
 set background=dark
 set ruler
 set ignorecase
+set encoding=utf-8
+set t_Co=256
+set termencoding=utf-8
+set number
